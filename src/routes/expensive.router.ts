26f7e2d4083579aa9ae77bb842647c55d2002e79
Filipe-barbosa <filipebarbosa94@hotmail.com>
2023-07-31 18:40:10 -0300

    fix: remove unuse character

--- src/routes/expensive.router.ts
+++ src/routes/expensive.router.ts
@@ -8,6 +8,6 @@ expensiveRouter.get('/',getAllExpensive)
 expensiveRouter.get('/:id',getExpensiveById)
 expensiveRouter.post('/',createExpensive)
 expensiveRouter.post('/:id',deleteExpensive)
-expensiveRouter.post('/:id:',updateExpensive)
+expensiveRouter.post('/:id',updateExpensive)
 
 export default expensiveRouter;
