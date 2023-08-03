2088ab1efe8b0e7c5477e6aa8e0b9a70b21c89bb
Raysson Ornelas <rayssonornelas2006@gmail.com>
2023-08-03 17:52:20 -0300

    enh:completion of balancing logic

--- src/controllers/balance.controller.ts
+++ src/controllers/balance.controller.ts
@@ -1,19 +1,24 @@
-import { getAllExpensive } from "./expensive.controller";
 import { Request, Response } from "express";
+import { expensive, PrismaClient, revenue } from '@prisma/client';
 
-const getExpensiveTotal = async () => {
-  const expensiveTotal = await getAllExpensive;
-  console.log(getAllExpensive)
-  return expensiveTotal;
+const getExpensiveTotal = (allExpensive: expensive[], allRevenues: revenue[]) => {
+  const expensiveTotal = allExpensive.reduce((total, expensive) => total + expensive.value, 0);
+  const revenueTotal = allRevenues.reduce((total, revenue) => total + revenue.value, 0);
+
+  return revenueTotal - expensiveTotal;
 }
+
+const expensiveClient = new PrismaClient().expensive;
+const revenueClient = new PrismaClient().revenue;
 
 export const getBalance = async (req: Request, res: Response): Promise<void> => {
   try {
-    const expensiveTotal = getExpensiveTotal();
+    const allExpensive: expensive[] = await expensiveClient.findMany();
+    const allRevenues: revenue[] = await revenueClient.findMany();
 
-    res.status(200).json({ data: expensiveTotal });
+    const total = getExpensiveTotal(allExpensive, allRevenues);
+    res.status(200).json({ data: total });
   } catch (error) {
     console.log(error);
   }
-  const expensiveTotal = getExpensiveTotal();
 }
